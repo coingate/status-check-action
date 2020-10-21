3f14fe2bbb3c04d2c667295bbbf9ee70f74cc6f7
minvs1 <achmedovas.tomas@gmail.com>
2020-10-21 18:20:37 +0300

    Init

---
+++ README.md
@@ -0,0 +1,38 @@
+# Status Check javascript action
+
+This action creates commit status check for given pull request.
+
+## Inputs
+
+### `github_token`
+
+**Required** GitHub token. (e.g. `secrets.GITHUB_TOKEN`).
+
+### `state`
+
+**Required** State of status check. (e.g. error, failure, pending, or success - [Read more](https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#create-a-commit-status--parameters)).
+
+### `owner`
+
+Repository owner. Default: current owner.
+
+### `repo`
+
+Repository name. Default: current repo.
+
+### `pull_number`
+
+Pull number to create status checks for. Default: current pull number.
+
+### `check_name`
+
+Name of the check. Default: current job.
+
+## Example usage
+
+```
+uses: coingate/status-check-action@main
+with:
+  github_token: '${{ secrets.GITHUB_TOKEN }}'
+  state: failure
+```
---
+++ src/index.js
@@ -0,0 +1,89 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+const githubToken = core.getInput("github_token");
+const owner =
+  core.getInput("owner") || github.context.payload.repository.owner.login;
+const repo = core.getInput("repo") || github.context.payload.repository.name;
+const checkName = core.getInput("check_name") || github.context.job;
+
+const octokit = github.getOctokit(githubToken);
+
+async function getCheckSuiteID() {
+  const runID = github.context.runId;
+
+  core.info(`Getting workflow with ID: ${runID}.`);
+
+  const { data: workflowRun } = await octokit.actions.getWorkflowRun({
+    owner,
+    repo,
+    run_id: runID,
+  });
+
+  return workflowRun.check_suite_id;
+}
+
+async function getLastCheckSuiteRunID(checkSuiteID) {
+  core.info(
+    `Getting check suite runs with ID: ${checkSuiteID} and check_name: "${checkName}".`
+  );
+
+  const {
+    data: { check_runs: checkRuns },
+  } = await octokit.checks.listForSuite({
+    owner,
+    repo,
+    check_suite_id: checkSuiteID,
+    check_name: checkName,
+  });
+
+  return checkRuns[checkRuns.length - 1].id;
+}
+
+async function getPullRequestHeadSha() {
+  const pullNumber =
+    core.getInput("pull_number") || github.context.payload.issue.number;
+
+  core.info(`Getting pull request: #${pullNumber}.`);
+
+  const { data: pullRequest } = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number: pullNumber,
+  });
+
+  return pullRequest.head.sha;
+}
+
+async function createCommitStatus(sha, checkRunID) {
+  const state = core.getInput("state");
+
+  core.info(`Creating commit status for SHA: "${sha}" with state: "${state}".`);
+
+  await octokit.repos.createCommitStatus({
+    owner,
+    repo,
+    sha,
+    state,
+    target_url: `https://github.com/coingate/cicd-test1/runs/${checkRunID}?check_suite_focus=true`,
+    context: checkName,
+  });
+}
+
+async function run() {
+  try {
+    core.info(`Starting for ${owner}/${repo}...`);
+
+    const checkSuiteID = await getCheckSuiteID();
+    const checkSuiteRunID = await getLastCheckSuiteRunID(checkSuiteID);
+
+    const prHeadSha = await getPullRequestHeadSha();
+    await createCommitStatus(prHeadSha, checkSuiteRunID);
+
+    core.info("Completed.");
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
